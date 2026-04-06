b6dfc73bfacc2404f87594c078559888f95d1565
ryeon-xx <rlarkgus3415@gmail.com>
2026-04-06 23:48:57 +0900

    feat : 가계부 입력폼 구현

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="app">
     <h1>가계부</h1>
+    <ExpenseForm @add-expense="addExpense"/>
   </div>
 </template>
 
 <script setup>
-  
+import ExpenseForm from './components/ExpenseForm.vue';
+import { ref } from 'vue';
+
+const expenses = ref([]) // 빈 배열로 시작
+
+const addExpense = (expense) => {
+  // expense 배열에 추가하려면?
+  expenses.value.push(expense)
+}
+
 </script>
---
+++ src/components/ExpenseForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="form">
+        <input v-model="description" type="text" placeholder="내용을 입력하세요."> &nbsp;
+        <input v-model="amount" type="number" placeholder="금액을 입력하세요."> &nbsp;
+        <select v-model="type">
+            <option value="income">수입</option>
+            <option value="expense">지출</option>
+        </select>
+        <button @click="handleSubmit()">추가</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const description = ref('');
+const amount = ref(0);
+const type = ref('expense');
+
+const emit = defineEmits(['add-expense'])
+
+const handleSubmit = () => {
+    emit('add-expense', {
+        description : description.value,
+        amount : amount.value,
+        type : type.value
+    })
+
+    // 초기화
+    description.value = ''
+    amount.value = 0
+    type.value = 'expense'
+}
+
+</script>
